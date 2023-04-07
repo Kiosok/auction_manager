cdbbafd0fc9570ce6c179be836b29d5a540c44db
Kiosok <john.lopatko.95@gmail.com>
2023-04-07 13:11:40 +0700

    Создал парсинг лота в единый словарь

--- parsing/parser.py
+++ parsing/parser.py
@@ -3,34 +3,74 @@ from selenium.webdriver.common.by import By
 
 # Здесь будем парсить сам лот
 with open('my_lot.html', 'r', encoding='utf-8') as file:
-    html_1 = file.read()
-with open('my_lot_public.html', 'r', encoding='utf-8') as file_2:
-    html_2 = file_2.read()
+    html = file.read()
+
+# Все данные с главной шапки
+dict_lot = {}
 
 
-def search_text(class_tag):
+def search_text(class_tag, name_teg=''):
     text_class = suop.find(class_=f'{class_tag}').text.strip()
-    return text_class
+    if 'Добавить в избранное' in text_class:
+        text_class = text_class.replace(' Добавить в избранное', '')
+
+    """Добавление в словарь"""
+    return {name_teg:text_class}
+
+
+def search_name_value(class_name, class_value):
+    keys = []
+    values = []
+
+    div_keys = suop.find_all(class_=f'{class_name}')
+    for i in div_keys:
+        keys.append(i.text.strip())
+
+    div_values = suop.find_all(class_=f'{class_value}')
+    for f in div_values:
+        values.append(f.text.strip())
+
+    dict_text = {k: v for k, v in zip(keys, values)}
+
+    return dict_text
+
+
+def replace_value_is_key(my_dict):
+    """Удаляет из ключа текст значения"""
+    for key, value in my_dict.items():
+        if value in key:
+            new_key = key.replace(value, '').strip()
+            my_dict[new_key] = my_dict.pop(key).strip()
+    return my_dict
 
 
 # меняем местами чтобы понимать что на обоих страницах работает
-html =  html_1 + html_2
-
 suop = BeautifulSoup(html, 'lxml')
 
-header_title = search_text('header_title')
-status = search_text('status').replace(' Добавить в избранное', '')
-lotbiddform = search_text('lotBiddForm with-small-bottom-gap')
-alert_content = search_text('alert-content')
-lot_number = search_text('lotAttributeValue')
-initial_price = search_text('prices__row__price-cell lotPrice')
-auction_step = suop.find_all(class_='prices__row__price-cell lotPrice')[1].text.strip()
-type_of_lease = suop.find_all(class_='lotAttributeValue')[1].text.strip()
+# Отдельные элименты
+lotbiddform = search_text('lotBiddForm with-small-bottom-gap', 'Форма заявки')
+status = search_text('status', 'Статус')
 
-print(lot_number)
+# Шапка
+dict_lot_handler = search_name_value('lotAttributeName', 'lotAttributeValue')
 
+# Все данные с информация о лоте
+dict_lot_information = search_name_value('attr_title', 'attr_value')
 
-# all_lot = suop.find_all('app-lot-list-item')
-#
-# for lot in all_lot:
-#     print(lot.find(class_='lotLink').get('href'))
+# Характеристики лота
+lot_characteristics = replace_value_is_key(search_name_value('char', 'lotAttributeDetail no-padding-bottom'))
+
+# Объединение в единый словарь
+dict_lot.update(dict_lot_handler)
+dict_lot.update(dict_lot_information)
+dict_lot.update(lot_characteristics)
+dict_lot.update(lotbiddform)
+dict_lot.update(status)
+
+print(dict_lot)
+
+# Ссылка на документы
+"""Изменить в предыдущем документе создания ссылок на доки файлов.
+Или впринципе создать отдельный метод на скачивание'Класс'. Который будет брать ссылку из класса лота приобразовывать её и скачивать"""
+# https://torgi.gov.ru/new/public/lots/lot/22000010620000000068_3/(lotInfo:docs)?fromRec=false#lotInfoSection-docs
+# https://torgi.gov.ru/new/public/lots/lot/22000010620000000068_3/(lotInfo:info)?fromRec=false#lotInfoSection-info
