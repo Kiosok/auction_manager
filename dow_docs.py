d8eac47530d51d0261cda74eb66c528b8c6d04ae
Kiosok <john.lopatko.95@gmail.com>
2023-04-19 15:48:12 +0700

    Отсчастья сделал скачивание файлов

--- dow_docs.py
+++ dow_docs.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import fake_useragent
 import os
@@ -10,7 +7,7 @@ import time
 import lxml
 import pickle
 
-url = "https://torgi.gov.ru/new/public/lots/lot/22000010210000000915_1/(lotInfo:docs)?fromRec=false#lotInfoSection-docs"
+url = "https://torgi.gov.ru/new/public/lots/lot/22000024510000000007_1/(lotInfo:docs)?fromRec=false#lotInfoSection-docs"
 # user agent
 user = fake_useragent.UserAgent().random
 # объект опций
@@ -18,7 +15,7 @@ options = webdriver.ChromeOptions()
 options.add_argument(f'"user-agent": {user}')
 
 # Загрузка файлов в назначенное место
-download_dir = '/Users/bender/PycharmProjects/auction_manager/docs/'
+download_dir = '/Users/bender/PycharmProjects/auction_manager/docs/Bur'
 options.add_experimental_option('prefs', {
     'download.default_directory': download_dir,
     'download.prompt_for_download': False,
@@ -27,7 +24,7 @@ options.add_experimental_option('prefs', {
 })
 
 # работа в фоновом режиме
-options.add_argument("headless")
+#options.add_argument("--headless=new")
 # Отключения режима webdriver
 # options.add_argument("--disable-blink-features=AutomationControlled")
 
@@ -41,10 +38,19 @@ try:
 
     time.sleep(5)
 
-    website_docs = driver.find_elements(By.CLASS_NAME, 'attachments-list__item__buttons__left-col')
+    website_docs = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-layout > main > app-work-area > div > div > ng-sidebar-container > div > div:nth-child(1) > app-public-shell > div > div > app-lot-item > div > div > div:nth-child(3) > div.work-space > div > div.tab-form-content > app-lot-docs > div > div:nth-child(4) > app-hashed-attachments-list > div > div:nth-child(1) > div > div.file-name-with-info > div.attachments-list__item__buttons__left-col > app-button.attachments-list__item__buttons__name.align-label-left.display-flex.text-button-no-center.word-break-caption")
+    print(website_docs)
+
+    # Find all elements with a class of "example-selector"
+    # объеденить с изначальным поиском элементов чтобы они страховали друг друга
+    website_docs = driver.find_elements(By.CSS_SELECTOR, ".word-break-caption")
     for dow_doc in website_docs:
+        time.sleep(1)
         dow_doc.click()
-    time.sleep(10)
+        time.sleep(1)
+
+
+    time.sleep(3)
     print('Документы скачены')
     downloaded_files = os.listdir(download_dir)
     print(downloaded_files)
